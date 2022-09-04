7e4b92fdfda0dd8bd34f6ca7e073b76ac685aec2
TuanNDA <tuannda@outlook.sa>
2022-09-04 17:27:39 +0700

    .

--- wp-content/themes/depchuanseo/header.php
+++ wp-content/themes/depchuanseo/header.php
@@ -28,33 +28,23 @@
 
     <header id="masthead" class="site-header">
         <div class="site-branding">
-            <?php
-            the_custom_logo();
-            if (is_front_page() && is_home()) :
-                ?>
+            <?php if (is_front_page() && is_home()) : ?>
                 <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-                <?php
-            else :
-                ?>
+            <?php else : ?>
                 <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-                <?php
-            endif;
-            $dcs_description = get_bloginfo('description', 'display');
-            if ($dcs_description || is_customize_preview()) :
-                ?>
-                <p class="site-description"><?php echo $dcs_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
             <?php endif; ?>
         </div><!-- .site-branding -->
 
         <nav id="site-navigation" class="main-navigation">
-            <a href="#primary-menu" class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'dcs'); ?></a>
+            <a href="#primary-menu" class="menu-toggle" aria-controls="primary-menu"
+               aria-expanded="false"><?php esc_html_e('Primary Menu', 'dcs'); ?></a>
             <?php
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'primary',
-                    'menu_id'        => 'primary-menu',
-                )
-            );
+            wp_nav_menu([
+                'theme_location' => 'primary',
+                'menu_id' => 'primary-menu',
+                'menu_class' => 'primary-menu',
+                'container' => false,
+            ]);
             ?>
         </nav><!-- #site-navigation -->
     </header><!-- #masthead -->
